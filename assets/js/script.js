72e464d46b90c751542019f2dc53c49529eee257
thaistardy - Trabalho <thaistardy.s@gmail.com>
2021-02-03 10:30:09 -0300

    Fazendo uma gambiarra no menu mobile, pois nao lembro como fazer essa droga de click no JS.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector('nav');
+let btnMenu = document.querySelector('.menu-opener');
+
+
+
+
+    function openMenu() {
+        menu.style.display = 'block';
+    }
+
+    function closeMenu() {
+        menu.style.display = 'none';
+    }
